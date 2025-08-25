d64df086fe4688468d99e60c1466aaae891cdd1d
jjuchan <yjc7241@naver.com>
2025-08-25 21:16:10 +0900

    setting

---
+++ src/main/java/com/team5_jsb/Team5JsbApplication.java
@@ -0,0 +1,13 @@
+package com.team5_jsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team5JsbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Team5JsbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/team5_jsb/Team5JsbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team5_jsb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team5JsbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
