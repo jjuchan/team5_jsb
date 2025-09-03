502846a0111bac22baa5831724162daca50f132f
jjuchan <yjc7241@naver.com>
2025-09-03 17:29:02 +0900

    feat: question mofifiydate 출력

--- src/main/java/com/team5_jsb/domain/question/question/dto/QuestionResponseDTO.java
+++ src/main/java/com/team5_jsb/domain/question/question/dto/QuestionResponseDTO.java
@@ -1,10 +1,10 @@
 package com.team5_jsb.domain.question.question.dto;
 
+import com.team5_jsb.domain.question.question.entity.Question;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.time.LocalDateTime;
-import com.team5_jsb.domain.question.question.entity.Question;
 
 @Data
 @AllArgsConstructor
@@ -13,16 +13,19 @@ public class QuestionResponseDTO {
     private String subject;
     private String content;
     private LocalDateTime createDate;
+    private LocalDateTime modifiedDate;
     private String authorUsername;
     private Integer viewCount;
     private Integer answerCount;
 
     public static QuestionResponseDTO fromEntity(Question question) {
+
         return new QuestionResponseDTO(
                 question.getId(),
                 question.getSubject(),
                 question.getContent(),
                 question.getCreatedDate(),
+                question.getModifiedDate(),
                 (question.getAuthor() != null ? question.getAuthor().getUsername() : null),
                 question.getViewCount(),
                 question.getAnswerList().size()
